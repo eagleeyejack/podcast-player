2de50ad9289a3c9b84112dca210a6f2c4ac0d450
Jack Evans <jackevans38@gmail.com>
2020-08-24 09:49:52 +0100

    Added error

--- src/components/Playbar/PlayerControls.js
+++ src/components/Playbar/PlayerControls.js
@@ -1,56 +1,46 @@
-import React from "react";
-import PlaybackBar from './PlaybackBar';
-import { useAppState } from '../../AppStateProvider';
-import { useAudioElement } from '../../AudioElementProvider';
-import { isDisabled } from '../../reducers/player';
-import {
-  PlayPauseButton,
-  PlayerButton
-} from "./buttons";
-import * as Icon from './buttons/icons';
-import useSkipControls from '../../hooks/useSkipControls';
-import useMediaSession from '../../hooks/useMediaSession';
+import React from "react"
+import PlaybackBar from "./PlaybackBar"
+import { useAppState } from "../../AppStateProvider"
+import { useAudioElement } from "../../AudioElementProvider"
+import { isDisabled } from "../../reducers/player"
+import { PlayPauseButton, PlayerButton } from "./buttons"
+import * as Icon from "./buttons/icons"
+import useSkipControls from "../../hooks/useSkipControls"
+import useMediaSession from "../../hooks/useMediaSession"
 
-export default function PlayerControls({currentTime, setShowAudioControls}) {
-  const { player } = useAppState();
-  const audioElement = useAudioElement();
-  const {
-    skipPrevious,
-    skipNext,
-    getPrevious,
-    getNext
-  } = useSkipControls();
-  useMediaSession();
+export default function PlayerControls({ currentTime, setShowAudioControls }) {
+  const { player } = useAppState()
+  const audioElement = useAudioElement()
+  const { skipPrevious, skipNext, getPrevious, getNext } = useSkipControls()
+  useMediaSession()
 
-  const disabled = isDisabled(player);
+  const disabled = isDisabled(player)
 
   function handlePlayPause() {
-    const audio = audioElement.current;
+    const audio = audioElement.current
     if (audio.paused) {
-      audio.play()
-        .catch(e => console.log('playing failed or was interrupted'));
+      audio.play().catch(e => console.log(e, "playing failed or was interrupted"))
     } else {
-      audio.pause();
+      audio.pause()
     }
   }
 
   function seekBackward() {
-    const audio = audioElement.current;
-    audio.currentTime = audio.currentTime - 15;
+    const audio = audioElement.current
+    audio.currentTime = audio.currentTime - 15
   }
 
   function seekForward() {
-    const audio = audioElement.current;
-    audio.currentTime = audio.currentTime + 30;
+    const audio = audioElement.current
+    audio.currentTime = audio.currentTime + 30
   }
 
-  const previous = getPrevious();
-  const next = getNext();
+  const previous = getPrevious()
+  const next = getNext()
 
   return (
     <div className="c-player-controls u-padding-left u-padding-right">
       <div className="c-player-controls__buttons u-padding-top-micro">
-
         <PlayerButton onClick={skipPrevious} disabled={!previous}>
           <Icon.SkipPrevious />
         </PlayerButton>
@@ -78,5 +68,5 @@ export default function PlayerControls({currentTime, setShowAudioControls}) {
         <PlaybackBar currentTime={currentTime} />
       </div>
     </div>
-  );
+  )
 }
